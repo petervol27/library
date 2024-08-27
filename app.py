1152a98d84c90ea1a1aecac0cd9b304b05cbdc83
petervol27 <petervol35@gmail.com>
2024-08-27 19:52:44 +0300

    oh god

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 from dotenv import load_dotenv
 import psycopg2
 from psycopg2.extras import DictCursor
-from back.connect import get_connection, create_tables
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -16,6 +16,32 @@ app.secret_key = os.getenv("secret_key")
 
 # development: http://127.0.0.1:9000/
 # production: https://library-klmc.onrender.com/
+def get_connection():
+    conn = psycopg2.connect(
+        host=os.getenv("db_host"),
+        database=os.getenv("db_name"),
+        user=os.getenv("db_user"),
+        password=os.getenv("db_password"),
+        port=os.getenv("db_port"),
+    )
+    return conn
+
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS books(id SERIAL PRIMARY KEY ,ISBN VARCHAR(20) UNIQUE ,name VARCHAR(150) NOT NULL,author VARCHAR(100) NOT NULL,quantity INT NOT NULL CHECK (quantity >= 0),img TEXT NOT NULL)"
+    )
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS readers(id SERIAL PRIMARY KEY ,name VARCHAR(50) NOT NULL,email VARCHAR(100) UNIQUE NOT NULL,password VARCHAR(100) NOT NULL,phone VARCHAR(20) UNIQUE)"
+    )
+    cursor.execute(
+        "CREATE TABLE IF NOT EXISTS rented(id SERIAL PRIMARY KEY ,bookId INT NOT NULL,readerId INT NOT NULL,rentDate DATE,returnDate DATE,FOREIGN KEY(bookId) REFERENCES books(id) ON DELETE CASCADE,FOREIGN KEY(readerId) REFERENCES readers(id) ON DELETE CASCADE) "
+    )
+    conn.commit()
+    conn.close()
+
 
 # SESSION CONTROL
 
